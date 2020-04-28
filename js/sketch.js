47c0fa1f631d0bd49aaa803125565e29926f31cd
Napass Masathienvong <nama4030@colorado.edu>
2020-04-29 02:03:35 +0700

    updated creative project p5.js files and code

--- js/sketch.js
+++ js/sketch.js
@@ -1,90 +1,74 @@
 
-let numBalls = 13;
-let spring = 0.05;
-let gravity = 0.03;
-let friction = -0.9;
-let balls = [];
+//variables
+var balls = []; // Particules array
+var mic; // Microphone library
+
 
 function setup() {
+  
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < numBalls; i++) {
-    balls[i] = new Ball(
-      random(width),
-      random(height),
-      random(30, 70),
-      i,
-      balls
-    );
-  }
-  noStroke();
-  fill(255, 204);
+  
+  //allow microphone
+  mic = new p5.AudioIn(); 
+  mic.start();
+  
+  for(i = 0; i < 0; i++){
+	  balls[i] = new ball();
+  } // create balls array
+  
+}
+
+//delete balls
+function mousePressed(){
+  
+	//balls.push(new ball(random(0, width), height)); // Add a new ball object
+  balls.splice(balls.length-1,1); // Delete the last ball object
 }
 
 function draw() {
-  background(0);
-  balls.forEach(ball => {
-    ball.collide();
-    ball.move();
-    ball.display();
-  });
+
+	textSize(25)
+	text("Make noise to generate, move mouse to change color and click to delete", windowWidth/2-400, 70);
+  
+  background(0,0,0,10);
+  
+  var vol = mic.getLevel();
+  if (vol > 0.1) {
+  balls.push(new ball(random(0, width), height));  
+  } // Create new ball object when the audio threshold level is crossed
+  
+  // RGB color with respect to mouse position on the canvas
+  var r = map(mouseY, 0, height, 0, 255);
+  var g = map(mouseX, 0, width, 0, 255);
+  var b = map(mouseY, 0, height, 0, 255);
+  fill(r, g, 255-b);
+  stroke(r, g, 255-b);
+  
+  // ball creation
+  for(i=0;i<balls.length;i++){
+  balls[i].display();
+  balls[i].move();
+  }
+    
 }
 
-class Ball {
-  constructor(xin, yin, din, idin, oin) {
-    this.x = xin;
-    this.y = yin;
-    this.vx = 0;
-    this.vy = 0;
-    this.diameter = din;
-    this.id = idin;
-    this.others = oin;
+function ball (X, Y){ // ball object
+  
+  this.x = random(0, width);
+  this.y = height;
+  
+  this.display = function(){
+    
+  	ellipse(this.x,this.y,20,20);
   }
-
-  collide() {
-    for (let i = this.id + 1; i < numBalls; i++) {
-      // console.log(others[i]);
-      let dx = this.others[i].x - this.x;
-      let dy = this.others[i].y - this.y;
-      let distance = sqrt(dx * dx + dy * dy);
-      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
-      //   console.log(distance);
-      //console.log(minDist);
-      if (distance < minDist) {
-        //console.log("2");
-        let angle = atan2(dy, dx);
-        let targetX = this.x + cos(angle) * minDist;
-        let targetY = this.y + sin(angle) * minDist;
-        let ax = (targetX - this.others[i].x) * spring;
-        let ay = (targetY - this.others[i].y) * spring;
-        this.vx -= ax;
-        this.vy -= ay;
-        this.others[i].vx += ax;
-        this.others[i].vy += ay;
-      }
-    }
+	this.move = function(){
+  	this.x = this.x + random(-0.5, 0.5);
+  	this.y = this.y - 1;
+    if (this.y > 0) {
+    	this.y = this.y - 1;
+  	} else {
+    	this.y = height;
+  	}
   }
-
-  move() {
-    this.vy += gravity;
-    this.x += this.vx;
-    this.y += this.vy;
-    if (this.x + this.diameter / 2 > width) {
-      this.x = width - this.diameter / 2;
-      this.vx *= friction;
-    } else if (this.x - this.diameter / 2 < 0) {
-      this.x = this.diameter / 2;
-      this.vx *= friction;
-    }
-    if (this.y + this.diameter / 2 > height) {
-      this.y = height - this.diameter / 2;
-      this.vy *= friction;
-    } else if (this.y - this.diameter / 2 < 0) {
-      this.y = this.diameter / 2;
-      this.vy *= friction;
-    }
-  }
-
-  display() {
-    ellipse(this.x, this.y, this.diameter, this.diameter);
-  }
+  
 }
